49735ea35671f931d739c09b4fa79ee213563ec4
yyi <i@yirannn.com>
2023-08-25 12:55:40 +0000

    add interface file for zswap

---
+++ sys/vm/zswap_interfaces.c
@@ -0,0 +1,7 @@
+// This file define those interfaces for migrating zswap from Linux
+// writed by modular-os-group.
+
+// This file include those interfaces:
+// Compress Module For Zswap (Write: Fan Yi)
+// Invoker Interface for FreeBSD (Write: Yi Ran)
+
---
+++ sys/vm/zswap_interfaces.h
@@ -0,0 +1,3 @@
+// This file declare those interfaces for migrating zswap from Linux
+// writed by modular-os-group.
+
