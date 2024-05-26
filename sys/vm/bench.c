f08fc65cf54ef117ba424aec0da0d622e2f60b57
yyi <i@yirannn.com>
2024-05-26 16:24:13 +0800

    1

--- sys/vm/bench.c
+++ sys/vm/bench.c
@@ -10,7 +10,7 @@
 char lorem[] =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque imperdiet felis et lobortis viverra. Aenean tristique nulla libero, vel gravida mauris dapibus ut. Integer laoreet sem non enim mollis, ut sollicitudin sapien vulputate. Aenean purus purus, facilisis et venenatis non, condimentum ut tortor. Quisque ac rhoncus enim. Duis laoreet pulvinar ultrices. Aenean vehicula est eu volutpat rutrum. Sed id feugiat quam. Sed nec tempus ligula, sed gravida nisl. Duis tincidunt rhoncus elementum. Aliquam erat volutpat. In odio mi, dapibus in tellus ut, rhoncus vulputate lectus.Ut a viverra purus.Nulla nisl tortor,dignissim quis dui pharetra,euismod rhoncus odio.Nullam eleifend neque non arcu sollicitudinmalesuada.Vivamus eget commodo ex,et aliquam ipsum.Pellentesque diam nibh, posuere non elementum vitae,consectetur feugiatmassa.Aliquam suscipit purus ut metus vestibulum laoreet.Mauris non    lacus lobortis,faucibus nibh non, euismod elit.Aliquam erat volutpat.Vestibulum sagittis,purus eget cursus cursus, sem tortor fringilla mauris,a pretium dolor eros in tellus.Duis sodales commodo sapien,nec malesuada mauris.Nam malesuada vestibulum magna.Aenean suscipit auctorsem,vitae convallis erat fermentum nec.Proin imperdiet pharetra malesuadaVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuerecubilia curae; Curabitur pretium est a eros blandit,sit amet tincidunt ante pulvinar.Donec sed dolor accumsan, aliquam purus id,tempor dolor.Sed fringilla sed risus ac eleifend.Phasellusin venenatis urna.Nullam consequat enim non ante fermentum,ac tristique dolor dictum.Fusce ut tellus malesuada, hendrerit neque tempus,hendrerit massa.Sed diam leo, venenatis a fringilla id,fringilla ut dui.Nunc id faucibus mauris,at pulvinar neque.Duisac maximus neque.Quisque sollicitudin venenatis nunc at luctus.Curabitur    nisl neque,pulvinar quis quam ac, rutrum tempor lectus.Fusce ex enim,pulvinar at dapibus eget, euismod eget sapien.Morbi euismod diam ante,non viverra ipsum vehicula ac.Vestibulum purus ante,sollicitudin id porttitor non,hendrerit quis urna Vestibulum ut ornare lectus";
 void
-dirty_memory(char *memory, size_t size, int cycle)
+dirty_memory(char *memory, size_t size)
 {
 	struct timespec start, end;
 	long seconds, nseconds;
@@ -38,7 +38,7 @@ dirty_memory(char *memory, size_t size, int cycle)
 // 读取并检查内存的正确性
 
 int
-verify_memory(char *memory, size_t size, int cycle)
+view_memory(char *memory, size_t size)
 {
 	struct timespec start, end;
 	long seconds, nseconds;
@@ -46,11 +46,8 @@ verify_memory(char *memory, size_t size, int cycle)
 	int cnt = 0;
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	for (size_t i = 0; i < size; i += sysconf(_SC_PAGESIZE)) {
-		if (memory[i] != (char)(cnt + cycle % 256)) {
-			printf(
-			    "Memory verification failed at page offset %zu, except : %u, actual : %u\n",
-			    i, (char)(cnt + cycle % 256), memory[i]);
-			// return 0; // 发现数据错误，返回0
+		if (memcmp(memory + i, lorem, sizeof(lorem)) != 0) {
+			printf("Error at page %d\n", cnt);
 		}
 		cnt++;
 	}
@@ -79,40 +76,20 @@ main(int argc, char *argv[])
 		return 1;
 	}
 
-	int cycles = 3; // 默认执行3轮
-	if (argc > 2) {
-		cycles = atoi(argv[2]);
-		if (cycles <= 0) {
-			fprintf(stderr,
-			    "Number of cycles must be a positive number\n");
-			return 1;
-		}
-	}
-
 	printf("Starting memory dirty and verify test...\n");
 
-	for (int i = 0; i < cycles; i++) {
-
-		size_t size = mb * 1024 * 1024; // MB转换为字节数
-		char *memory = malloc(size);
-		if (memory == NULL) {
-			perror("Failed to allocate memory");
-			return 1;
-		}
-
-		memset(memory, -1, size); // 清零分配的内存
-
-		printf("Cycle %d:\n", i + 1);
-		dirty_memory(memory, size, i);
-		if (!verify_memory(memory, size, i)) {
-			printf("Memory verification failed in cycle %d\n",
-			    i + 1);
-			free(memory);
-			return 1;
-		}
-
-		free(memory);
+	size_t size = mb * 1024 * 1024; // MB转换为字节数
+	char *memory = malloc(size);
+	if (memory == NULL) {
+		perror("Failed to allocate memory");
+		return 1;
 	}
+
+	memset(memory, -1, size); // 清零分配的内存
+
+	dirty_memory(memory, size);
+	view_memory(memory, size);
+	free(memory);
 
 	printf("Test completed successfully.\n");
 	return 0;
