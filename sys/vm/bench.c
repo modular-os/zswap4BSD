a91794b2a2cab04ad56693cfe2d76e9625003d0e
yyi <i@yirannn.com>
2024-05-26 17:27:31 +0800

    1

--- sys/vm/bench.c
+++ sys/vm/bench.c
@@ -18,7 +18,7 @@ dirty_memory(char *memory, size_t size, int cycle)
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	int cnt = 0;
 	for (size_t i = 0; i < size; i += sysconf(_SC_PAGESIZE)) {
-		memory[i] = (char)(cnt + cycle % 256);
+		memory[i] = 0xab;
 		cnt++;
 		if (cnt % 100000 == 0) {
 			printf("now at %d page\n", cnt);
@@ -44,7 +44,7 @@ verify_memory(char *memory, size_t size, int cycle)
 	int cnt = 0;
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	for (size_t i = 0; i < size; i += sysconf(_SC_PAGESIZE)) {
-		if (memory[i] != (char)(cnt + cycle % 256)) {
+		if (memory[i] != 0xab) {
 			printf(
 			    "Memory verification failed at page offset %zu, except : %u, actual : %u\n",
 			    i, (char)(cnt + cycle % 256), memory[i]);
