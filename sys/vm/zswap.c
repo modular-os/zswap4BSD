29c018c7b57d4aa63375fe523159510ca0c26a40
yyi <i@yirannn.com>
2024-07-16 12:42:07 +0000

    add benchmark

--- sys/vm/zswap.c
+++ sys/vm/zswap.c
@@ -1,4 +1,5 @@
 
+#include <sys/types.h>
 #include <asm/atomic.h>
 #include <linux/highmem.h>
 #include <linux/kernel.h>
@@ -17,6 +18,7 @@
 
 #include "frontswap.h"
 #include "linux/gfp.h"
+#include "stdlib.h"
 #include "sys/md5.h"
 #include "sys/types.h"
 #include "vm/uma.h"
@@ -1530,8 +1532,18 @@ zswap_init(void)
 late_initcall(zswap_init);
 
 #include <sys/sysproto.h>
+#include <sys/time.h>
 
-enum { OP_INIT = 0, OP_SWAP_STORE = 1, OP_SWAP_LOAD = 2, OP_EXIT = 3 };
+enum { OP_INIT = 0, OP_SWAP_STORE = 1, OP_SWAP_LOAD = 2 };
+
+void create_page_by_random_percent(caddr_t virt_addr, int percent) {
+	size_t total_size = PAGE_SIZE;
+	size_t random_size = PAGE_SIZE * percent / 100;
+	size_t same_size = total_size - random_size;
+
+	memset(virt_addr, 'A', same_size);
+	arc4random(virt_addr + same_size, random_size);
+}
 int
 sys_zswap_interface(struct thread *td, struct zswap_interface_args *uap)
 {
@@ -1539,56 +1551,41 @@ sys_zswap_interface(struct thread *td, struct zswap_interface_args *uap)
 	unsigned type = 0;
 	pgoff_t offset = 100;
 	bool exi = false;
-
-	MD5_CTX ctx;
-	u_char digest[MD5_DIGEST_LENGTH];
+	struct timespec start_time, end_time, delta_time;
 
 	struct page *my_page = alloc_page(GFP_KERNEL);
-	switch (uap->cmd) {
-	case OP_INIT:
-		// error = init_zbud();
-		// if(error != 0) return (error);
-		printf("Start Test Init In Kernel\n");
-		error = zswap_init();
-		zswap_frontswap_init(0);
-		// check_enter_module();
-		if (error != 0)
-			return (error);
-		break;
-	case OP_SWAP_STORE:
-		printf("Start Test Store In Kernel\n");
-		// make a new random page
-		vm_paddr_t phys_addr = VM_PAGE_TO_PHYS(my_page);
-		caddr_t virt_addr = (caddr_t)PHYS_TO_DMAP(phys_addr);
-		for (size_t i = 0; i < PAGE_SIZE; i += 64) {
-			arc4random_buf(virt_addr + i, 16);
-			memset(virt_addr + i + 16, 0, 48);
-		}
-		peek(virt_addr, 16, "rand buf");
-		// arc4random_buf(virt_addr, PAGE_SIZE);
-		// get hexdigest for the page
-		MD5Init(&ctx);
-		MD5Update(&ctx, virt_addr, PAGE_SIZE);
-		MD5Final(digest, &ctx);
-		peek(digest, MD5_DIGEST_LENGTH, "storing md5");
-		int res = zswap_frontswap_store(type, offset, my_page);
-		printf("store res : %d\n", res);
-		memset(virt_addr, 0, PAGE_SIZE);
-		break;
-
-	case OP_SWAP_LOAD:
-		// bool exi = false;
-		zswap_frontswap_load(type, offset, my_page, &exi);
-		vm_paddr_t phys_addr_1 = VM_PAGE_TO_PHYS(my_page);
-		caddr_t virt_addr_1 = (caddr_t)PHYS_TO_DMAP(phys_addr_1);
-		peek(virt_addr_1, 16, "loaded buf");
-		MD5Init(&ctx);
-		MD5Update(&ctx, virt_addr_1, PAGE_SIZE);
-		MD5Final(digest, &ctx);
-		peek(digest, MD5_DIGEST_LENGTH, "loaded md5");
-		break;
-	case OP_EXIT:
-		break;
+	vm_paddr_t phys_addr = VM_PAGE_TO_PHYS(my_page);
+	caddr_t virt_addr = (caddr_t)PHYS_TO_DMAP(phys_addr);
+	
+	create_page_by_random_percent(virt_addr, uap->cmd);
+	// record time
+	nanouptime(&start_time);
+	for(int i = 0; i < 50000; i ++ ) {
+		zswap_frontswap_store(type, i, my_page);
 	}
+	nanouptime(&end_time);
+
+	timespecsub(&end_time, &start_time, &delta_time);
+	double total_time_seconds = (delta_time.tv_sec + delta_time.tv_nsec / 1e9);
+	printf("Store took %ld microseconds, with qps %.0f\n", (long)(delta_time.tv_sec * 1000000 + delta_time.tv_nsec / 1000), 50000/total_time_seconds);
+	// print time per store & qps
+
+	// record time
+	nanouptime(&start_time);
+	for(int i = 0; i < 100000; i ++ ) {
+		// 1 : 9 store & load
+		int ifstore = (arc4random() % 10) < 1;
+
+		if(ifstore) {
+			zswap_frontswap_store(type, arc4random() % 50000, my_page);
+		} else {
+			zswap_frontswap_load(type, arc4random() % 50000, my_page, &exi);
+		}
+	}
+	nanouptime(&end_time);
+
+	timespecsub(&end_time, &start_time, &delta_time);
+	double total_time_seconds = (delta_time.tv_sec + delta_time.tv_nsec / 1e9);
+	printf("Operation took %ld microseconds, with qps %.0f\n", (long)(delta_time.tv_sec * 1000000 + delta_time.tv_nsec / 1000), 100000/total_time_seconds);
 	return 0;
 }
